a2f4968d936a2123de5eddc25a5f323f6b3a5f3c
pakets16 <konstantin45rus@icloud.com>
2025-02-24 18:42:33 +0500

    fix jail

---
+++ scripts/f2b_roters_kss.sh
@@ -0,0 +1,4 @@
+#!bin /bash
+apt install fail2ban
+echo -e "[sshd]\nenabled = true\nport = 22\nfilter = sshd\nlogpath = /var/log/auth.log\nmaxretry = 3\nbantime = 600\nignoreip = 10.10.100.10/24" >> /etc/fail2ban/jail.conf
+systemctl restart fail2ban
